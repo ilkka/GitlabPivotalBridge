e5e2b3ec1135d5d99fdf6a7300e5f3a454649efb
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2013-10-10 17:07:40 +0300

    Drop shellscript server preparation in favor of puppet
    The problems with sudo were insurmountable, so I just dropped that
    approach, instead pushing a puppet manifest onto the server and then
    running that. There is only one pure ssh hack and that is to install
    puppet itself.

--- prepare-servers.sh
+++ prepare-servers.sh
@@ -2,74 +2,6 @@
 # Prepare server(s) for deployment
 set -e
 
-function has_puppet {
-  local HOST=$1
-  ssh $HOST "which puppet" > /dev/null
-}
-
-function install_puppet {
-  local HOST=$1
-  local SUDOPASS=$2
-  echo $SUDOPASS | ssh $HOST "cd /tmp && wget http://apt.puppetlabs.com/puppetlabs-release-precise.deb && sudo -p '' dpkg -i puppetlabs-release-precise.deb && sudo apt-get update && sudo env DEBIAN_FRONTEND=noninteractive apt-get install -y puppet"
-}
-
-function has_git {
-  local HOST=$1
-  ssh $HOST "which git" > /dev/null
-}
-
-function install_git {
-  local HOST=$1
-  local SUDOPASS=$2
-  echo $SUDOPASS | ssh $HOST "sudo -p '' env DEBIAN_FRONTEND=noninteractive apt-get install -y git"
-}
-
-function has_deploy_user {
-  local HOST=$1
-  ssh $HOST "grep -q deploy /etc/passwd"
-}
-
-function create_deploy_user {
-  local HOST=$1
-  local SUDOPASS=$2
-  echo $SUDOPASS | ssh $HOST "sudo -p '' adduser --disabled-password --gecos deploy,,, deploy"
-}
-
-function can_ssh_as_deploy_user {
-  local HOST=$1
-  ssh -i $HOME/.ssh/id_rsa deploy@$HOST true
-}
-
-function add_ssh_key {
-  local HOST=$1
-  local SUDOPASS=$2
-  (echo $SUDOPASS; cat $HOME/.ssh/id_rsa.pub) | ssh $HOST "sudo -p '' mkdir -p /home/deploy/.ssh && sudo chown deploy:deploy /home/deploy/.ssh && sudo chmod 700 /home/deploy/.ssh && sudo tee /home/deploy/.ssh/authorized_keys"
-}
-
-function has_sudoers_line {
-  local HOST=$1
-  local SUDOPASS=$2
-  echo $SUDOPASS | ssh $HOST "sudo -p '' grep -q deploy-setup-tag /etc/sudoers"
-}
-
-function add_sudoers_line {
-  local HOST=$1
-  local SUDOPASS=$2
-  echo $SUDOPASS | ssh $HOST "sudo -p '' cp /etc/sudoers /tmp/sudoers.tmp && echo '# Automatically added, do not edit. deploy-setup-tag'|sudo tee -a /tmp/sudoers.tmp && echo 'deploy ALL = (ALL:ALL) NOPASSWD: SETENV: /usr/bin/puppet, /sbin/start, /sbin/stop, /sbin/restart'|sudo tee -a /tmp/sudoers.tmp && sudo visudo -cf /tmp/sudoers.tmp && sudo cp /etc/sudoers /etc/sudoers.bak && sudo mv /tmp/sudoers.tmp /etc/sudoers"
-}
-
-function is_opt_writable {
-  local HOST=$1
-  local SUDOPASS=$2
-  echo $SUDOPASS | ssh $HOST "sudo -p '' su deploy -c 'touch /opt/.deploy-can-write' && sudo rm /opt/.deploy-can-write"
-}
-
-function make_opt_writable {
-  local HOST=$1
-  local SUDOPASS=$2
-  echo $SUDOPASS | ssh $HOST "sudo -p '' chgrp deploy /opt && sudo chmod g+rwxs /opt"
-}
-
 HOSTS=()
 
 while  (( "$#" )); do
@@ -79,32 +11,48 @@ done
 
 for HOST in $HOSTS; do
   echo "Preparing $HOST"
-  sudopass=""
-  read -s -p "sudo password for $HOST:" sudopass
-  echo
-  if ! has_puppet $HOST > /dev/null; then
-    echo "Installing puppet on $HOST from apt.puppetlabs.com"
-    install_puppet $HOST $sudopass > /dev/null
-  fi
-  if ! has_git $HOST > /dev/null; then
-    echo "Installing git on $HOST"
-    install_git $HOST $sudopass > /dev/null
-  fi
-  if ! has_deploy_user $HOST > /dev/null; then
-    echo "Creating deploy user on $HOST"
-    create_deploy_user $HOST $sudopass > /dev/null
-  fi
-  if ! can_ssh_as_deploy_user $HOST > /dev/null; then
-    echo "Adding $HOME/.ssh/id_rsa.pub to authorized_keys for deploy user on $HOST"
-    add_ssh_key $HOST $sudopass > /dev/null
-  fi
-  if ! has_sudoers_line $HOST $sudopass > /dev/null; then
-    echo "Adding sudoers line for deploy user on $HOST"
-    add_sudoers_line $HOST $sudopass > /dev/null
-  fi
-  if ! is_opt_writable $HOST > /dev/null; then
-    echo "Making /opt writable for deploy"
-    make_opt_writable $HOST $sudopass > /dev/null
-  fi
+  scp $HOME/.ssh/id_rsa.pub $HOST:/tmp/deploy-pubkey
+  ssh $HOST "which puppet" || ssh -t $HOST "cd /tmp && wget http://apt.puppetlabs.com/puppetlabs-release-precise.deb && sudo dpkg -i puppetlabs-release-precise.deb && sudo apt-get update && sudo env DEBIAN_FRONTEND=noninteractive apt-get install -y puppet"
+  ssh $HOST "cat > /tmp/prepare-server.pp" <<EOF
+package { "git":
+  ensure => installed,
+}
+user { "deploy":
+  ensure => present,
+  home => "/home/deploy"
+}
+file { "/home/deploy":
+  require => User["deploy"],
+  ensure => directory,
+  mode => 0755,
+  owner => "deploy"
+}
+file { "/home/deploy/.ssh":
+  ensure => directory,
+  mode => 0700,
+  owner => "deploy"
+}
+file { "/home/deploy/.ssh/authorized_keys":
+  ensure => file,
+  owner => "deploy"
+}
+exec { "add-pubkey":
+  require => File["/home/deploy/.ssh/authorized_keys"],
+  cwd => "/home/deploy",
+  user => "deploy",
+  path => ["/usr/local/sbin", "/usr/local/bin", "/usr/sbin", "/usr/bin", "/sbin", "/bin"],
+  command => "cat /tmp/deploy-pubkey >> .ssh/authorized_keys"
+}
+exec { "add-sudoers-line":
+  path => ["/usr/local/sbin", "/usr/local/bin", "/usr/sbin", "/usr/bin", "/sbin", "/bin"],
+  command => "grep -q deploy-setup-tag /etc/sudoers || cp /etc/sudoers /tmp/sudoers.tmp && echo '# Automatically added, do not edit. deploy-setup-tag' >> /tmp/sudoers.tmp && echo 'deploy ALL = (ALL:ALL) NOPASSWD: SETENV: /usr/bin/puppet, /sbin/start, /sbin/stop, /sbin/restart' >> /tmp/sudoers.tmp && visudo -cf /tmp/sudoers.tmp && cp /etc/sudoers /etc/sudoers.bak && mv /tmp/sudoers.tmp /etc/sudoers"
+}
+file { "/opt":
+  ensure => directory,
+  mode => "u+rwx,g+rwxs,o+rx",
+  group => "deploy"
+}
+EOF
+  ssh -t $HOST "sudo puppet apply /tmp/prepare-server.pp"
   echo "$HOST ready for deployment"
 done
