38b65a38bd45415cf6446635c4bf633df5c5324a
Ilkka Laukkanen <ilkka.laukkanen@futurice.com>
2013-10-10 10:52:01 +0300

    Handle play framework installation with puppet

--- manifests/bridge.pp
+++ manifests/bridge.pp
@@ -1,4 +1,44 @@
 # vim: ft=puppet
+package { "curl":
+  ensure => installed
+}
+
+user { "gitlab-bridge":
+  ensure => present
+}
+
+exec { "download-play":
+  require => [Package["curl"], User["gitlab-bridge"]],
+  cwd => "/home/gitlab-bridge",
+  command => "/usr/bin/curl -O http://downloads.typesafe.com/play/2.2.0/play-2.2.0.zip",
+  creates => "/home/gitlab-bridge/play-2.2.0.zip",
+  user => "gitlab-bridge"
+}
+
+package { "unzip":
+  ensure => installed
+}
+
+exec { "unzip-play": 
+  require => [Package["unzip"], Exec["download-play"]],
+  cwd => "/home/gitlab-bridge",
+  command => "/usr/bin/unzip play-2.2.0.zip",
+  creates => "/home/gitlab-bridge/play-2.2.0",
+  user => "gitlab-bridge"
+}
+
+file { "/home/gitlab-bridge/play":
+  ensure => link,
+  target => "/home/gitlab-bridge/play-2.2.0"
+}
+
+exec { "add-play-to-path":
+  require => File["/home/gitlab-bridge-play"],
+  cwd => "/home/gitlab-bridge",
+  command => "echo 'export PATH=$HOME/play:$PATH # gl-play-path-setup' >> .bashrc",
+  unless => "grep -q gl-play-path-setup .bashrc"
+}
+
 file { "/etc/init/gitlabpivotalbridge.conf":
   ensure => file,
   content => template("${app_path}/manifests/upstart.conf.erb")
